e0309064fbc9fcbbec7dc9c8040a0adc635f8fca
PilipovichArina <122990514+PilipovichArina@users.noreply.github.com>
2024-12-20 22:22:26 +0300

    Update test_square.py

--- tests/test_square.py
+++ tests/test_square.py
@@ -1,41 +1,38 @@
 import unittest
-from square import area, perimeter
+from math import pi
+from circle import area, perimeter
 
 
-class TestAreaSquare(unittest.TestCase):
-
+class TestCircle(unittest.TestCase):
     def test_area(self):
-        a = 5
-        expected_area = a * a
-        result = area(a)
-        self.assertAlmostEqual(result, expected_area)
+        radius = 1
+        res = area(radius)
+        self.assertEqual(res, pi)
 
-    def test_area_invalid_side(self):
-        invalid_side = [0, -1, -100]
-        for side in invalid_side:
-            with self.subTest(side=side):
-                with self.assertRaises(ValueError):
-                    area(side)
+    def test_perimeter(self):
+        radius = 1
+        res = perimeter(radius)
+        self.assertEqual(res, 2 * pi)
 
+    def test_area_zero(self):
+        radius = 0
+        res = area(radius)
+        self.assertEqual(res, 0)
 
-class TestPerimeterSquare(unittest.TestCase):
+    def test_perimeter_zero(self):
+        radius = 0
+        res = perimeter(radius)
+        self.assertEqual(res, 0)
 
-    def test_perimetr(self):
-        a = 7
-        expected_perimetr = 4 * a
-        result = perimeter(a)
-        self.assertAlmostEqual(result, expected_perimetr)
+    def test_area_neg(self):
+        radius = -1
+        with self.assertRaises(AssertionError):
+            area(radius)
 
-    def test_perimeter_invalid_side(self):
-        invalid_side = [0, -1, -100]
-        for side in invalid_side:
-            with self.subTest(side=side):
-                with self.assertRaises(ValueError):
-                    perimeter(side)
-
-
-if __name__ == '__main__':
-    unittest.main()
+    def test_perimeter_neg(self):
+        radius = -1
+        with self.assertRaises(AssertionError):
+            perimeter(radius)
 
 
 if __name__ == '__main__':
